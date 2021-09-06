ac4315f84a567a771fb38047cc9717e4d7e57458
Alexandr Karensky <skittles8bit@gmail.com>
2021-09-06 22:41:30 +0300

    feat: added project

---
+++ NewsFeedApp/NewsFeedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsFeedApp
+//
+//  Created by Alexander Karenski on 6.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
